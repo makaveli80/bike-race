7b7c86cf827598d64268e3b22a65621349fadf16
Makaveli80 <tom.briois@gmail.com>
2020-08-03 16:57:14 +0200

    Implementation of middleware validating a racer to add

--- src/Racers/AddRacer/AddRacer.reducer.js
+++ src/Racers/AddRacer/AddRacer.reducer.js
@@ -1,10 +1,36 @@
 export const ADD_RACER = 'ADD_RACER';
+export const ADD_RACER_ERROR = 'ADD_RACER_ERROR';
 
 export const addRacer = (racer) => {
-  return {
-    type: ADD_RACER,
-    payload: racer
-  };
+  return { type: ADD_RACER, payload: racer };
+}
+
+export const addRacerError = (errors) => {
+  return { type: ADD_RACER_ERROR, payload: errors };
+}
+
+export const addRacerValidationMiddleware = ({ dispatch }) => next => action => {
+  if (action.type !== ADD_RACER) {
+    return next(action);
+  }
+
+  const errors = validateRacer(action.payload);
+  return errors.length > 0
+    ? dispatch(addRacerError(errors))
+    : next(action);
+};
+
+const validateRacer = (racer) => {
+  const errors = [];
+
+  if (!racer.firstName) {
+    errors.push("Prénom du coureur obligatoire")
+  }
+  if (!racer.lastName) {
+    errors.push("Nom du coureur obligatoire")
+  }
+
+  return errors;
 }
 
 export const addRacerReduce = (racers, racer) => {
--- src/Racers/AddRacer/AddRacer.reducer.test.js
+++ src/Racers/AddRacer/AddRacer.reducer.test.js
@@ -1,20 +1,98 @@
-import { addRacerReduce } from './AddRacer.reducer';
+import {
+  ADD_RACER,
+  ADD_RACER_ERROR,
+  addRacer,
+  addRacerError,
+  addRacerValidationMiddleware,
+  addRacerReduce
+} from './AddRacer.reducer';
 
 const EMPTY_RACERS = [];
-const FILLED_RACERS = [{
-  lastName: 'Bachelet',
-  firstName: 'Corentin'
-}];
-const NEW_RACER = {
-  lastName: 'Briois',
-  firstName: 'Jean-Luc'
-}
+const FILLED_RACERS = [{ firstName: 'Corentin', lastName: 'Bachelet' }];
+const NEW_RACER = { firstName: 'Jean-Luc', lastName: 'Briois' };
+const EMPTY_RACER = {};
+const RACER_WITHOUT_FIRSTNAME = { lastName: 'Briois' };
+const RACER_WITHOUT_LASTNAME = { firstName: 'Jean-Luc' };
+
+describe('addRacer', () => {
+  it('should return an "add racer" action', () => {
+    // when
+    const action = addRacer(NEW_RACER);
+    // then
+    expect(action).toEqual({ type: ADD_RACER, payload: expect.anything() });
+  });
+})
+
+describe('addRacerError', () => {
+  it('should return an "add racer error" action', () => {
+    // when
+    const action = addRacerError(['Erreur']);
+    // then
+    expect(action).toEqual({ type: ADD_RACER_ERROR, payload: expect.anything() });
+  });
+})
+
+describe('addRacerValidationMiddleware', () => {
+  const fakeStore = { dispatch: jest.fn() };
+  const fakeNext = jest.fn();
+
+  it('should pass through when not an "ADD_RACER" type action', () => {
+    // given
+    const action = { type: "FAKE_TYPE" };
+    // when
+    addRacerValidationMiddleware(fakeStore)(fakeNext)(action);
+    // then
+    expect(fakeNext).toHaveBeenCalledWith(action);
+  });
+
+  it('should dispatch an "ADD_RACER_ERROR" type action when racer empty', () => {
+    // given
+    const action = { type: ADD_RACER, payload: EMPTY_RACER };
+    // when
+    addRacerValidationMiddleware(fakeStore)(fakeNext)(action);
+    // then
+    expectAddRacerErrorDispatched();
+  });
+
+  it('should dispatch an "ADD_RACER_ERROR" type action when racer wihtout first name', () => {
+    // given
+    const action = { type: ADD_RACER, payload: RACER_WITHOUT_FIRSTNAME };
+    // when
+    addRacerValidationMiddleware(fakeStore)(fakeNext)(action);
+    // then
+    expectAddRacerErrorDispatched();
+  });
+
+  it('should dispatch an "ADD_RACER_ERROR" type action when racer wihtout last name', () => {
+    // given
+    const action = { type: ADD_RACER, payload: RACER_WITHOUT_LASTNAME };
+    // when
+    addRacerValidationMiddleware(fakeStore)(fakeNext)(action);
+    // then
+    expectAddRacerErrorDispatched();
+  });
+
+  it('should pass through when racer valid', () => {
+    // given
+    const action = { type: ADD_RACER, payload: NEW_RACER };
+    // when
+    addRacerValidationMiddleware(fakeStore)(fakeNext)(action);
+    // then
+    expect(fakeNext).toHaveBeenCalledWith(action);
+  });
+
+  const expectAddRacerErrorDispatched = () => {
+    expect(fakeStore.dispatch).toHaveBeenCalledWith(
+      expect.objectContaining({ type: ADD_RACER_ERROR })
+    );
+  }
+});
+
 
 describe('addRacerReduce', () => {
   it('should add a new racer to an empty list of racers', () => {
     // when
     const racersUpdated = addRacerReduce(EMPTY_RACERS, NEW_RACER);
-
     // then
     expect(racersUpdated.length).toBe(1);
     expect(racersUpdated).toContain(NEW_RACER);
@@ -23,9 +101,8 @@ describe('addRacerReduce', () => {
   it('should add a new racer to an already filled list of racers', () => {
     // when
     const racersUpdated = addRacerReduce(FILLED_RACERS, NEW_RACER);
-
     // then
     expect(racersUpdated.length).toBe(2);
     expect(racersUpdated).toContain(NEW_RACER);
-  })
+  });
 });
