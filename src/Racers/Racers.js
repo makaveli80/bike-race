513e88eb8234405fa26af3e25e2433f78b642d78
Makaveli80 <tom.briois@gmail.com>
2020-08-31 08:55:03 +0200

    Integration of ListRacers into app

--- src/Racers/Racers.js
+++ src/Racers/Racers.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import AddRacer from './AddRacer/AddRacer';
+import ListRacers from './ListRacers/ListRacers';
 import { addRacer } from './AddRacer/AddRacer.reducer';
 
-export const Racers = ({ addRacer }) => {
+export const Racers = ({ addRacer, racers }) => {
   return (
     <div>
       <AddRacer onSubmit={(racer) => addRacer(racer)}/>
+      <ListRacers racers={racers}/>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {};
+const mapStateToProps = (state) => {
+  return {
+    racers: state.racers
+  }
+};
 
 export default connect(mapStateToProps, {
   addRacer
