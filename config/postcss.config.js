5d4392eb79402af71cdfb9deda7b1fc429fe885a
Makaveli80 <tom.briois@gmail.com>
2020-09-14 09:12:06 +0200

    Added a Tailwindcss config file with future features

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('tailwindcss')('./config/tailwind.config.js'),
     require('postcss-nesting'),
     require('autoprefixer')
   ]
