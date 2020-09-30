5c109da06c7ba12f3bac0a3981541ae799084a28
Makaveli80 <tom.briois@gmail.com>
2020-09-30 07:51:59 +0200

    Migrating to atrule-bem postcss plugin

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-atrule-bem'),
     require('tailwindcss')('./config/tailwind.config.js'),
     require('postcss-extend-rule'),
     require('postcss-nesting'),
