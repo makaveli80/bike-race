6ef6d9d1f3f2428256f4da8e26238acb18111c51
Makaveli80 <tom.briois@gmail.com>
2020-10-15 08:27:09 +0200

    Adding new rules to racersNavigation selector when a search is present

--- src/Racers/FilterRacers/racersNavigation.selector.js
+++ src/Racers/FilterRacers/racersNavigation.selector.js
@@ -2,15 +2,39 @@ import _ from 'lodash';
 
 import getFilteredRacersSelector from './filteredRacers.selector';
 
-const getRacersNavigationSelector = ({racers, racersFilter}) => {
-  const filteredRacers = getFilteredRacersSelector({racers, racersFilter});
-  const currentIndex = filteredRacers.length !== 0 ? racersFilter.indexPage : 0;
-  const totalPages = _.ceil(racers.length / racersFilter.racersPerPage);
+const getCurrentPageProperties = (filteredRacers, racersFilter) => {
+  const currentIndex = filteredRacers.length !== 0
+    ? racersFilter.indexPage
+    : 0;
+
   return {
     currentIndex,
-    totalPages,
-    totalFilteredRacers: filteredRacers.length,
-    totalRacers: racers.length,
+    totalFilteredRacers: filteredRacers.length
+  }
+}
+
+const getTotalPagesProperties = (racers, filteredRacers, racersFilter) => {
+  const totalRacers = _.isEmpty(racersFilter.searchedWord)
+    ? racers.length
+    : filteredRacers.length;
+    
+  return {
+    totalRacers,
+    totalPages: _.ceil(totalRacers / racersFilter.racersPerPage)
+  }
+}
+
+const getRacersNavigationSelector = ({racers, racersFilter}) => {
+  const filteredRacers = getFilteredRacersSelector({racers, racersFilter});
+
+  const { currentIndex, totalFilteredRacers } = getCurrentPageProperties(filteredRacers, racersFilter);
+  const { totalRacers, totalPages } = getTotalPagesProperties(racers, filteredRacers, racersFilter);
+
+  return {
+    currentIndex,
+    totalFilteredRacers,
+    totalRacers,
+    totalPages
   }
 }
 
