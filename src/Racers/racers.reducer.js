49ca3c088cedd2d8a34400277b4980ad2065643e
Makaveli80 <tom.briois@gmail.com>
2020-10-15 08:36:17 +0200

    Removing initial list of racers

--- src/Racers/racers.reducer.js
+++ src/Racers/racers.reducer.js
@@ -1,9 +1,6 @@
 import { ADD_RACER, addRacerReduce } from './AddRacer/addRacer.reducer';
 
-const INITIAL_RACERS = [
-  { firstName: 'Jean-Luc', lastName: 'Briois' },
-  { firstName: 'Corentin', lastName: 'Bachelet' }
-];
+const INITIAL_RACERS = [];
 
 export const racersReducer = (state = INITIAL_RACERS, action) => {
   switch (action.type) {
